e670c104e9cbbdd4e0330375a675955ddde2ace3
AFUKUCHI <fkc45449@gmail.com>
2018-11-07 20:05:00 +0800

    変更がうまく反映されてなかったので再度コミット

--- signup.php
+++ signup.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
     //SESSIONの有効化
@@ -6,12 +5,12 @@
 
     // POST送信があった場合
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $first_name=htmlspecialchars($_POST['first_name']);
-        $last_name=htmlspecialchars($_POST['last_name']);
-        $email=htmlspecialchars($_POST['email']);
-        $passward=htmlspecialchars($_POST['passward']);
-        $passward_check=htmlspecialchars($_POST['passward_check']);
-        $user_type=$_POST['user_type'];
+        $first_name = htmlspecialchars($_POST['first_name']);
+        $last_name = htmlspecialchars($_POST['last_name']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        $password_check = htmlspecialchars($_POST['password_check']);
+        $user_type = $_POST['user_type'];
 
         //性が未入力の場合
         if ($first_name == '') {
@@ -32,32 +31,32 @@
         }
 
         //パスワードが未入力の場合
-        if ($passward == '') {
-            $passward_msg = 'パスワードを入力して下さい';
-            $validations['passward'] = '未入力';
+        if ($password == '') {
+            $password_msg = 'パスワードを入力して下さい';
+            $validations['password'] = '未入力';
         }
 
         //確認用パスワードが未入力の場合
-        if ($passward_check == '') {
-            $passward_check_msg = 'パスワードを入力して下さい';
-            $validations['passward_check'] = '未入力';
+        if ($password_check == '') {
+            $password_check_msg = 'パスワードを入力して下さい';
+            $validations['password_check'] = '未入力';
         }
 
         // パスワードと確認用パスワードが一致しない場合またはパスワードが4~8文字の英数字でない場合
-        $passward_length = strlen($passward);
-        $passward_int = preg_match('/[0-9]/', $passward);
-        $passward_alpha = preg_match('/[a-zA-Z]/', $passward);
-        $passward_int_alpha = preg_match('/[^0-9a-zA-Z]/', $passward);
-        if ($passward != '' && $passward_check != '') {
+        $password_length = strlen($password);
+        $password_int = preg_match('/[0-9]/', $password);
+        $password_alpha = preg_match('/[a-zA-Z]/', $password);
+        $password_int_alpha = preg_match('/[^0-9a-zA-Z]/', $password);
+        if ($password != '' && $password_check != '') {
 
 
             //パスワードと確認用パスワードが一致しない場合
-            if ($passward !== $passward_check) {
+            if ($password !== $password_check) {
                 $unmatch_msg = 'パスワードが一致しません';
                 $validations['unmatch'] = '不一致';
 
             //パスワードが4~8文字の英数字でない場合
-            } elseif ($passward_length < 4 || $passward_length > 8 || $passward_int == FALSE || $passward_alpha == FALSE || $passward_int_alpha == TRUE) {
+            } elseif ($password_length < 4 || $password_length > 8 || $password_int == FALSE || $password_alpha == FALSE || $password_int_alpha == TRUE) {
                 $unqualified_msg = 'パスワードは4~8文字の英数字で入力して下さい';
                 $validations['unqualified'] = '文字数';
             }
@@ -68,7 +67,7 @@
             $_SESSION['EATY']['first_name']  = $first_name;
             $_SESSION['EATY']['last_name']  = $last_name;
             $_SESSION['EATY']['email']  = $email;
-            $_SESSION['EATY']['passward']  = $passward;
+            $_SESSION['EATY']['password']  = $password;
             $_SESSION['EATY']['user_type']  = $user_type;
             header('Location: check.php');
             exit();
@@ -87,16 +86,14 @@
         $first_name = '';
         $last_name = '';
         $email = '';
-        $passward = '';
-        $passward_check = '';
+        $password = '';
+        $password_check = '';
         $radio_t = 'checked';
         $radio_s = '';
     }
 
 ?>
 
-=======
->>>>>>> parent of 22f8135... PHPの機能を付加
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -127,35 +124,56 @@
     <p class="title font-weight-bold">新規登録</p>
     <form class="signup_form" method="POST" action="">
 
-    <!-- Text input-->
-    <div class="form-group" >
-      <input name="first_name" type="text" placeholder="姓" class="form-control" style="width:200px; display: inline-block; ">
-    </div>
-
       <!-- Text input-->
-      <div class="form-group">
-        <input name="last_name" type="text" placeholder="名" class="form-control" style="width:200px; display: inline-block;">
+      <div class="form-group" >
+        <input name="first_name" type="text" placeholder="姓" class="form-control" style="width:200px; display: inline-block;" value="<?=$first_name ?>">
+        <?php if(isset($validations['first_name'])): ?>
+          <span style="color:red;"><?=$first_name_msg ?></span>
+        <?php endif; ?>
       </div>
 
       <!-- Text input-->
       <div class="form-group">
-        <input name="email" type="text" placeholder="メールアドレス" class="form-control" style="width:200px; display: inline-block;">
+        <input name="last_name" type="text" placeholder="名" class="form-control" style="width:200px; display: inline-block;" value="<?=$last_name ?>" >
+        <?php if(isset($validations['last_name'])): ?>
+          <span style="color:red;"><?=$last_name_msg ?></span>
+        <?php endif; ?>
+      </div>
+
+      <!-- Text input-->
+      <div class="form-group">
+        <input name="email" type="text" placeholder="メールアドレス" class="form-control" style="width:200px; display: inline-block;" value="<?=$email ?>">
+        <?php if(isset($validations['email'])): ?>
+          <span style="color:red;"><?=$email_msg ?></span>
+        <?php endif; ?>
       </div>
 
       <!-- Password input-->
       <div class="form-group">
-        <input name="passward" type="password" placeholder="パスワード" class="form-control" style="width:200px; display: inline-block;">
+        <input name="password" type="password" placeholder="パスワード" class="form-control" style="width:200px; display: inline-block;">
+        <?php if(isset($validations['password'])): ?>
+          <span style="color:red;"><?=$password_msg ?></span>
+        <?php endif; ?>
+        <?php if(isset($validations['unmatch'])): ?>
+          <span style="color:red;"><?=$unmatch_msg ?></span>
+        <?php endif; ?>
+        <?php if(isset($validations['unqualified'])): ?>
+          <span style="color:red;"><?=$unqualified_msg ?></span>
+        <?php endif; ?>
       </div>
 
       <!-- Password input-->
       <div class="form-group">
-        <input name="passward_check" type="password" placeholder="パスワード再入力" class="form-control" style="width:200px; display: inline-block;">
+        <input name="password_check" type="password" placeholder="パスワード再入力" class="form-control" style="width:200px; display: inline-block;">
+        <?php if(isset($validations['password_check'])): ?>
+          <span style="color:red;"><?=$password_check_msg ?></span>
+        <?php endif; ?>
       </div>
 
       <div class="form-group">
-        <input type="radio" name="user_type" id="type-0" value="講師" checked="checked">
+        <input type="radio" name="user_type" id="type-0" value="講師" <?=$radio_t ?>>
         講師
-        <input type="radio" name="user_type" id="type-1" value="生徒">
+        <input type="radio" name="user_type" id="type-1" value="生徒" <?=$radio_s ?>>
         生徒
       </div>
 
