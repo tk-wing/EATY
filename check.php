e37d272abb03c1ba6402b3852810e28cc6ca95dc
Tsubasa Kudo <>
2018-11-08 11:34:36 +0800

    性⇛姓へ変更

--- check.php
+++ check.php
@@ -83,7 +83,7 @@
 
     <div class="check">
       <h3 class="text-success"><?=$type ?></h3>
-      <p class="check_content"><?=$last_name . ' ' . $first_name ?></p>
+      <p class="check_content"><?=$first_name . ' ' . $last_name ?></p>
       <p class="check_content"><?=$email ?></p>
       <p class="check_content">●●●●●●●●</p>
     </div>
@@ -114,6 +114,31 @@
       <p>©ex chef</p>
     </div>
   </footer>
+
+  <div class="modal fade" id="demoNormalModal" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="demoModalTitle">タイトル</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+              <div class="modal-body">
+                  内容・・・
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                  <a href="card.html"><button type="button" class="btn btn-primary">ボタン</button></a>
+              </div>
+          </div>
+      </div>
+  </div>
+  
+  <!-- jQuery、Popper.js、Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
 </body>
