dc851135166761f888524b1af7b9498e97939084
LAPTOP-J8V82213\Fukumoto Etsuko <motsu_net@yahoo.co.jp>
2018-11-13 10:18:27 +0800

    ファイル作成！

---
+++ bkg_comfirm.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>予約内容確認</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <!-- BootstrapのCSS読み込み -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- jQuery読み込み -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- BootstrapのJS読み込み -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- FontAwesome読み込み -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+
+</head>
+<body>
+  <header>
+    <div class="text-center">
+      <a href="#"><img src="img/logo/logo.jpg" width="90"></a>
+    </div>
+  </header>
+
+  <div class="text-left container">
+    <form class="bkg_form" method="POST" action="">
+      <p class="title front-weight-bold">レッスン名</p>
+      <p class="title front-weight-bold">日時</p>
+      <p class="title front-weight-bold">料金</p>
+      <p class="title front-weight-bold">支払方法</p>
+      <p class="title front-weight-bold">お名前</p>
+      <p class="title front-weight-bold">メールアドレス</p>
+      <p class="title front-weight-bold">メッセージ</p>
+      <input type="submit" value="この内容で予約" class="btn btn-primary" style="width:200px;"><input type="submit" value="予約内容を変更" class="btn btn-primary" style="width:200px;">
+    </form>
+  </div>
+
+ 
+  <footer>
+    <div class="sns text-center">
+      <a href="" class="btn-facebook sns-btn"><i class="fab fa-facebook fa-2x"></i></a>
+      <a href="" class="btn-twitter sns-btn"><i class="fab fa-twitter fa-2x"></i></a>
+      <a href="" class="btn-instagram sns-btn"><i class="fab fa-instagram fa-2x"></i></a>
+      <p>©ex chef</p>
+    </div>
+  </footer>
+
+
+</body>
+</html>
