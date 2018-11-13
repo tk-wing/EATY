bedf4eaba67bd710a298bcb135287251bc85c085
川東勇介 <>
2018-11-14 00:52:45 +0800

    頑張ってます＾＾

--- serch_s.php
+++ serch_s.php
@@ -6,7 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+   <link href=>
   <link rel="stylesheet" href="css/stylesheet_serch.css">
+  <link rel="stylesheet" href="css/stylesheet_serch_s.css">
+
     <!-- BootstrapのCSS読み込み -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <!-- jQuery読み込み -->
@@ -31,27 +34,212 @@
       <a href="#"><img src="img/eatylogo.png" width="100"></a>
     </div>
   </header>
-  <a href="">検査条件をクリア</a>
-  <div>
-    <div>
-      <form action="serch_s.php" method="POST">
-        <dl>
-          <dt>日付</dt>
-          <dd>
-            <select id="genre" name="genre" class="form-control">
-              <option value="1">Option one</option>
-              <option value="2">Option two</option>
-            </select>
-          </dd>
-        </dl>
-      </form>
+
+
+ <a href="">検査条件をクリア</a>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<div class="container text-center">
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark">
+
+            <div class="">
+                <ul class="navbar-nav">
+                  日付
+                  <li class="nav-item dropdown">
+                    <select id="genre" name="genre" class="form-control">
+                      <option value="1">Option one</option>
+                      <option value="2">Option two</option>
+                    </select>
+                    </li>
+                    開催場所
+                    <li class="nav-item dropdown">
+                       <select id="genre" name="genre" class="form-control">
+                         <option value="1">Option one</option>
+                         <option value="2">Option two</option>
+                       </select>
+                    </li>
+                    ジャンル
+                    <li class="nav-item dropdown">
+                      <select id="genre" name="genre" class="form-control">
+                        <option value="1">Option one</option>
+                        <option value="2">Option two</option>
+                      </select>
+                    </li>
+                    キーワード
+                    <li class="nav-item dropdown">
+                      <select id="genre" name="genre" class="form-control">
+                        <option value="1">Option one</option>
+                        <option value="2">Option two</option>
+                      </select>
+                    </li>
+                        <!-- <input type="submit" class="btn btn-primary" value="レッスン検索"> -->
+                </ul>
+            </div>
+                <input type="submit" class="btn btn-primary" value="レッスン検索">
+    </nav>
+</div>
+
+<!-- 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+ -->
+
+    <div class="container">
+      <div id="products" class="row view-group">
+
+                <div class="item col-xs-4 col-lg-4">
+
+                    <div class="thumbnail card">
+                      <!-- 全体の白い枠 -->
+                      <h4 class="group card-title inner list-group-item-heading">簡単オムライス</h4>
+
+                        <!-- div class="img-event">
+                            <img class="group list-group-image img-fluid" src="http://placehold.it/400x250/000/fff" alt="" />
+                        </div> -->
+
+                        <div class="blog-inner">
+                          <img class="img-responsive" src="http://placehold.jp/350x200.png" alt="Blog">
+                        </div>
+                        <div class="caption card-body">
+                          <!-- 下の白い枠 -->
+                             <table>
+                               <div>
+                                 <td>開催場所</td>
+                                 <td>¥3000/1人</td>
+                               </div>
+                               <tr>
+                                 <td>画像</td>
+                                 <td>講師名</td>
+                                 <td>
+                                    <a class="btn btn-success" href="http://localhost/batch46/EATY/serch_s.php">レッスン詳細へ</a>
+                                 </td>
+                               </tr>
+                             </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="item col-xs-4 col-lg-4">
+                    <div class="thumbnail card">
+                        <div class="img-event">
+                            <img class="group list-group-image img-fluid" src="http://placehold.it/400x250/000/fff" alt="" />
+                        </div>
+                        <div class="caption card-body">
+                            <h4 class="group card-title inner list-group-item-heading">
+                                激ムズオムライス</h4>
+                            <p class="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6">
+                                    <p class="lead">
+                                        $21.000</p>
+                                </div>
+                                <div class="col-xs-12 col-md-6">
+                                    <a class="btn btn-success" href="http://www.jquery2dotnet.com">レッスン詳細へ</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item col-xs-4 col-lg-4">
+                    <div class="thumbnail card">
+                        <div class="img-event">
+                            <img class="group list-group-image img-fluid" src="http://placehold.it/400x250/000/fff" alt="" />
+                        </div>
+                        <div class="caption card-body">
+                            <h4 class="group card-title inner list-group-item-heading">
+                                Product title</h4>
+                            <p class="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6">
+                                    <p class="lead">
+                                        $21.000</p>
+                                </div>
+                                <div class="col-xs-12 col-md-6">
+                                    <a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item col-xs-4 col-lg-4">
+                    <div class="thumbnail card">
+                        <div class="img-event">
+                            <img class="group list-group-image img-fluid" src="http://placehold.it/400x250/000/fff" alt="" />
+                        </div>
+                        <div class="caption card-body">
+                            <h4 class="group card-title inner list-group-item-heading">
+                                Product title</h4>
+                            <p class="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6">
+                                    <p class="lead">
+                                        $21.000</p>
+                                </div>
+                                <div class="col-xs-12 col-md-6">
+                                    <a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item col-xs-4 col-lg-4">
+                    <div class="thumbnail card">
+                        <div class="img-event">
+                            <img class="group list-group-image img-fluid" src="http://placehold.it/400x250/000/fff" alt="" />
+                        </div>
+                        <div class="caption card-body">
+                            <h4 class="group card-title inner list-group-item-heading">
+                                Product title</h4>
+                            <p class="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6">
+                                    <p class="lead">
+                                        $21.000</p>
+                                </div>
+                                <div class="col-xs-12 col-md-6">
+                                    <a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item col-xs-4 col-lg-4">
+                    <div class="thumbnail card">
+                        <div class="img-event">
+                            <img class="group list-group-image img-fluid" src="http://placehold.it/400x250/000/fff" alt="" />
+                        </div>
+                        <div class="caption card-body">
+                            <h4 class="group card-title inner list-group-item-heading">
+                                Product title</h4>
+                            <p class="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6">
+                                    <p class="lead">
+                                        $21.000</p>
+                                </div>
+                                <div class="col-xs-12 col-md-6">
+                                    <a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
-  </div>
-
-
-
-
-
 
 
 
