084ad1606b12ee72ca1a0d982052824e16b687e5
川東勇介 <>
2018-11-21 10:01:57 +0800

    placeをstationに変更

--- create_check_t.php
+++ create_check_t.php
@@ -10,39 +10,43 @@
     if (!isset($_SESSION['eaty'])) {
       header('Location: create_lesson.php');
     }
+
+
+
+
     v($_POST,'$_POST');
     v($_SESSION,'$SESSION');
 
-    $day = $_SESSION['EATY']['day'];
-    $daytime = $_SESSION['EATY']['daytime'];
-    $place = $_SESSION['EATY']['place'];
-    $fee = $_SESSION['EATY']['fee'];
-    $requiretime = $_SESSION['EATY']['requiretime'];
-    $category_id = $_SESSION['EATY']['category_id'];
-    $menu = $_SESSION['EATY']['menu'];
-    $capacity = $_SESSION['EATY']['capacity'];
-    $basic = $_SESSION['EATY']['basic'];
-    $lesson_name = $_SESSION['EATY']['lesson_name'];
-    $menudetail =  $_SESSION['EATY']['menudetail'];
-    $bring = $_SESSION['EATY']['bring'];
-    $precaution = $_SESSION['EATY']['precaution'];
+    $day = $_SESSION['eaty']['day'];
+    $daytime = $_SESSION['eaty']['daytime'];
+    $station = $_SESSION['eaty']['station'];
+    $fee = $_SESSION['eaty']['fee'];
+    $requiretime = $_SESSION['eaty']['requiretime'];
+    $category_id = $_SESSION['eaty']['category_id'];
+    $menu = $_SESSION['eaty']['menu'];
+    $capacity = $_SESSION['eaty']['capacity'];
+    $basic = $_SESSION['eaty']['basic'];
+    $lesson_name = $_SESSION['eaty']['lesson_name'];
+    $menudetail =  $_SESSION['eaty']['menudetail'];
+    $bring = $_SESSION['eaty']['bring'];
+    $precaution = $_SESSION['eaty']['precaution'];
     //画像
-    $img_1 = $_SESSION['EATY']['img_1'];
-    $img_2 = $_SESSION['EATY']['img_2'];
-    $img_3 = $_SESSION['EATY']['img_3'];
-    $img_4 = $_SESSION['EATY']['img_4'];
+    $img_1 = $_SESSION['eaty']['img_1'];
+    $img_2 = $_SESSION['eaty']['img_2'];
+    $img_3 = $_SESSION['eaty']['img_3'];
+    $img_4 = $_SESSION['eaty']['img_4'];
 
       if (!empty($_POST)) {
         //DBへの入力
-        $sql = 'INSERT INTO `lessons_t` SET `img_1`=?,`img_2`=?,`img_3`=?,`img_4`=?,`day`=?,`daytime`=?,`place`=?,`fee`=?,`requiretime`=?,`category_id`=?,`menu`=?,`capacity`=?,`basic`=?,`lesson_name`=?,`menudetail`=?,`bring`=?,`precaution`=?,`user_id`=?,`created`= NOW()';
-        $data =array($img_1,$img_2,$img_3,$img_4,$day,$daytime,$place,$fee,$requiretime,$category_id,$menu,$capacity,$basic,$lesson_name,$menudetail,$bring,$precaution,$_SESSION['id']);
+        $sql = 'INSERT INTO `lessons_t` SET `img_1`=?,`img_2`=?,`img_3`=?,`img_4`=?,`day`=?,`daytime`=?,`station`=?,`fee`=?,`requiretime`=?,`category_id`=?,`menu`=?,`capacity`=?,`basic`=?,`lesson_name`=?,`menudetail`=?,`bring`=?,`precaution`=?,`created`= NOW()';//`user_id`=?,
+        $data =array($img_1,$img_2,$img_3,$img_4,$day,$daytime,$station,$fee,$requiretime,$category_id,$menu,$capacity,$basic,$lesson_name,$menudetail,$bring,$precaution);//$_SESSION['id']
         $stmt = $dbh->prepare($sql);
         $stmt->execute($data);
 
-        unset($_SESSION['eaty']);
+        unset($_SESSION['eaty']);//一度空に
 
-        header('Location: bkg.php');
-        exit();
+        // header('Location: bkg.php');
+        // exit();
       }
       
     
@@ -82,20 +86,21 @@
     </div>
   </header>
 
+  <!-- <form method="POST" action="" enctype="multipart/form-data"> -->
   <div class="create_check_content text-center">
     <div class="blog-inner-prof text-center">
       <h3>レッスン名:<?= h($lesson_name);?></h3>
-      <img class="lesson_img" src="<?= h($img_1);?>" style="width:300px;height:200px;">
-      <img class="lesson_img" src="<?= h($img_2);?>" style="width:300px;height:200px;"><br>
-      <img class="lesson_img" src="<?= h($img_3);?>" style="width:300px;height:200px;">
-      <img class="lesson_img" src="<?= h($img_4);?>" style="width:300px;height:200px;">
+      <img class="lesson_img" src="users_lesson_img/<?= h($img_1);?>" style="width:300px;height:200px;">
+      <img class="lesson_img" src="users_lesson_img/<?= h($img_2);?>" style="width:300px;height:200px;"><br>
+      <img class="lesson_img" src="users_lesson_img/<?= h($img_3);?>" style="width:300px;height:200px;">
+      <img class="lesson_img" src="users_lesson_img/<?= h($img_4);?>" style="width:300px;height:200px;">
 
       <div class="row contents">
           <div class="col-md-4">
             <span><i class="far fa-calendar-alt fa-2x icon"></i>日時:<?= h($day);?></span>
           </div>
           <div class="col-md-4">
-            <span><i class="fas fa-train fa-2x icon"></i>最寄り駅:<?= h($place);?></span>
+            <span><i class="fas fa-train fa-2x icon"></i>最寄り駅:<?= h($station);?></span>
           </div>
           <div class="col-md-4">
             <span><i class="fas fa-yen-sign fa-2x icon"></i>料金:<?= h($fee);?></span>
@@ -140,15 +145,14 @@
             </div>
           </li>
         </ul>
-
-        <form method="POST" action="">
-          <input type="submit" class="btn btn-primary" value="完了">
-          <a href="#"><button type="button" class="btn btn-secondary">編集</button></a>
-        </form>
+          <form method="POST" action="" enctype="multipart/form-data">
+            <input type="submit" class="btn btn-primary" value="完了">
+            <a href="create_lesson.php"><button type="button" class="btn btn-secondary">編集</button></a>
+          </form>
 
     </div>
   </div>
-
+  <!-- </form> -->
   <footer>
     <div class="sns text-center">
       <a href="" class="btn-facebook sns-btn"><i class="fab fa-facebook fa-2x"></i></a>
