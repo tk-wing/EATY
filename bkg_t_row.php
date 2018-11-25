e1e79bfe5ba56b40ccce130081f4c31ece3201af
川東勇介 <>
2018-11-26 03:48:14 +0800

    rowを作成。繰り返しの部分は分けた。これから編集もするので増やしました。

---
+++ bkg_t_row.php
@@ -0,0 +1,31 @@
+
+<div class="blog-inner-prof">
+    <div class="row">
+      <div class="col-md-2 text-center">
+        <p><?php echo $lessons_each["day"]; ?></p>
+        <p><?php echo $lessons_each["daytime"]; ?></p>
+      </div>
+
+      <div class="col-md-2 text-center">
+        <p><?php echo $lessons_each["lesson_name"]; ?></p>
+      </div>
+
+      <div class="col-md-2 text-center">
+        <p><?php echo $lessons_each["station"]; ?></p>
+      </div>
+
+      <div class="col-md-2 text-center">
+        <p><?php echo $lessons_each["capacity"]; ?></p>
+      </div>
+
+      <div class="col-md-2 text-center">
+        <p><?php echo $lessons_each["status"]; ?></p>
+      </div>
+
+      <div class="col-md-2 text-center">
+        <a href="#"><button type="button" class="btn btn-primary">レッスン詳細</button></a>
+        <a href="#"><button type="button" class="btn btn-primary">レッスン編集</button></a>
+      </div>
+
+    </div>
+</div>
