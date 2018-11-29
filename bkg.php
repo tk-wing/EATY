b77022a1a4d737b4ccd7e2749bbca5b5c50b9542
LAPTOP-J8V82213\Fukumoto Etsuko <motsu_net@yahoo.co.jp>
2018-11-29 09:38:52 +0800

    アクセス中のレッスンNOの取得

--- bkg.php
+++ bkg.php
@@ -2,6 +2,8 @@
     session_start();
     require('dbconnect.php');
     require('functions.php');
+    $lesson_id = $_GET['lesson_id'];
+    $user_type = '';
     // $sql = 'SELECT * FROM `profiles_s` WHERE `user_id`=?';
     // $data = array($_SESSION['EATY']['id']);
 
@@ -20,7 +22,7 @@
     $stmt->execute($data);
     $signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $sql = 'SELECT * FROM `lessons_t` WHERE `id`="10"';
+    $sql = 'SELECT * FROM `lessons_t` WHERE `id`=?';
     $data = array($_SESSION['EATY']['id']);
 
     var_dump($_SESSION['EATY']['id']);
