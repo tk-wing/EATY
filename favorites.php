92b5f95328790b9b859914508057b8de84770bf9
Tsubasa Kudo <>
2018-11-29 11:06:52 +0800

    お気に入りレッスンの表示

--- favorites.php
+++ favorites.php
@@ -4,6 +4,56 @@
     require('dbconnect.php');
     require('functions.php');
 
+    // ユーザー情報を取得
+    $sql='SELECT * FROM `users` WHERE `id`=?';
+    $stmt = $dbh->prepare($sql);
+    $data = array($_SESSION['EATY']['id']);
+    $stmt->execute($data);
+
+    $signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // プロフィール情報をを取得
+    $profile_s_sql='SELECT * FROM `profiles_s` WHERE `user_id`=?';
+    $profile_s_stmt = $dbh->prepare($profile_s_sql);
+    $profile_s_sql_data = [$signin_user['id']];
+    $profile_s_stmt->execute($profile_s_sql_data);
+    $profile_s = $profile_s_stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($profile_s != FALSE){
+        if (empty($profile_s['nickname'])) {
+            $name = $signin_user['last_name'] . '　' . $signin_user['first_name'];
+        } else {
+            $name = $profile_s['nickname'];
+        }
+
+        if (!empty($profile_s['img_name'])) {
+            $img_name = $profile_s['img_name'];
+        } else {
+            $img_name = '';
+        }
+    }else{
+        $name = $signin_user['last_name'] . '　' . $signin_user['first_name'];
+        $img_name = '';
+    }
+
+    // お気に入りレッスン情報をを取得
+    $favorite_sql='SELECT `l`.*, `f`.* FROM `lessons_t` AS `l` INNER JOIN `like_lessons` AS `f` ON `l`.`id` = `f`.`lesson_id` WHERE `f`.`user_id`=? ORDER BY `l`.`day` DESC';
+    $favorite_stmt = $dbh->prepare($favorite_sql);
+    $favorite_sql_data = [$signin_user['id']];
+    $favorite_stmt->execute($favorite_sql_data);
+
+    $favorites = [];
+
+    while(1){
+        $favorite = $favorite_stmt->fetch(PDO::FETCH_ASSOC);
+        if ($favorite == FALSE) {
+            break;
+        }
+        $favorites[] = $favorite;
+    }
+
+
+
 
 
 
@@ -14,7 +64,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <title>つくれぽ一覧</title>
+  <title>お気に入りレッスン一覧</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -48,124 +98,43 @@
 
   <div class="wrapper">
     <div class="top-content text-center">
-       <img class="rounded-circle" src="img/profile_img_defult.png" style="width:120px;height:120px;">
-       <p>〇〇さんのつくれぽ</p>
+        <?php if ($img_name == ''): ?>
+          <img class="rounded-circle" src="img/profile_img_defult.png" style="width:120px;height:120px;">
+        <?php else: ?>
+          <img class="rounded-circle" src="user_profile_img/<?php echo $img_name ?>" style="width:120px;height:120px;">
+        <?php endif ?>
+       <p><?php echo $name ?>さんのお気入りレッスン</p>
     </div>
 
     <div class="row text-center" style="border: 4px solid #2C373B;border-radius: 240px 15px 185px 15px / 15px 200px 15px 185px;margin: 2em 0;padding: 2em;">
 
-      <div class="col-md-12 text-center">お気に入りレッスンはまだありません。</div>
+      <?php if (empty($favorites)): ?>
+        <div class="col-md-12 text-center">お気に入りレッスンはまだありません。</div>
+      <?php else: ?>
+        <?php foreach ($favorites as $favorite): ?>
+          <div class="col-md-3 text-center">
+            <span>レッスン日：<?php echo date('m月d日', strtotime($favorite['day'])); ?></span>
+            <div class="blog-inner" style="border:solid 1px; ">
+              <img class="img-responsive" src="users_lesson_img/<?php echo $favorite['img_1']; ?>" alt="Blog"  width="100%" style="height: 200px;">
+              <p><?php echo $favorite['lesson_name'] ?></p>
 
+                  <form method="POST" action="">
+                    <div class="row">
+                      <div class="col" style="padding: 0px">
+                        <a href="#"><button type="button" class="btn btn-warning" style="font-size: 13px">お気に入りから削除</button></a>
+                      </div>
+                      <div class="col">
+                        <a href="lesson.php?lesson_id=<?php echo $favorite['lesson_id'] ?>"><button type="button" class="btn btn-primary" style="font-size: 13px">レッスン詳細ページへ</button></a>
+                      </div>
+                    </div>
+                  </form>
 
-      <div class="col-md-3 text-center">
-        <span>日付</span>
-        <div class="blog-inner" style="border:solid 1px; ">
-          <img class="img-responsive" src="user_report_img/<?php echo $report_each['img_name']; ?>" alt="Blog" style="margin: 5px">
-          <p style="text-align: left; padding: 3px 3px"><?php echo $report_each['feed']; ?></p>
-          <div class="desc">
-
-            <form method="POST" action="">
-              <div class="row">
-                <div class="col" style="padding: 0px">
-                  <a href="#"><button type="button" class="btn btn-warning" style="font-size: 13px">お気に入りから削除</button></a>
-                </div>
-                <div class="col">
-                  <a href="#"><button type="button" class="btn btn-primary" style="font-size: 13px">レッスン詳細ページへ</button></a>
-                </div>
               </div>
-            </form>
+            </div>
+        <?php endforeach ?>
+      <?php endif ?>
 
-          </div>
-        </div>
-        </div>
 
-        <div class="col-md-3 text-center">
-        <span>日付</span>
-        <div class="blog-inner" style="border:solid 1px; ">
-          <img class="img-responsive" src="user_report_img/<?php echo $report_each['img_name']; ?>" alt="Blog" style="margin: 5px">
-          <p style="text-align: left; padding: 3px 3px"><?php echo $report_each['feed']; ?></p>
-          <div class="desc">
-
-            <form method="POST" action="">
-              <div class="row">
-                <div class="col" style="padding: 0px">
-                  <a href="#"><button type="button" class="btn btn-warning" style="font-size: 13px">お気に入りから削除</button></a>
-                </div>
-                <div class="col">
-                  <a href="#"><button type="button" class="btn btn-primary" style="font-size: 13px">レッスン詳細ページへ</button></a>
-                </div>
-              </div>
-            </form>
-
-          </div>
-        </div>
-        </div>
-
-        <div class="col-md-3 text-center">
-        <span>日付</span>
-        <div class="blog-inner" style="border:solid 1px; ">
-          <img class="img-responsive" src="user_report_img/<?php echo $report_each['img_name']; ?>" alt="Blog" style="margin: 5px">
-          <p style="text-align: left; padding: 3px 3px"><?php echo $report_each['feed']; ?></p>
-          <div class="desc">
-
-            <form method="POST" action="">
-              <div class="row">
-                <div class="col" style="padding: 0px">
-                  <a href="#"><button type="button" class="btn btn-warning" style="font-size: 13px">お気に入りから削除</button></a>
-                </div>
-                <div class="col">
-                  <a href="#"><button type="button" class="btn btn-primary" style="font-size: 13px">レッスン詳細ページへ</button></a>
-                </div>
-              </div>
-            </form>
-
-          </div>
-        </div>
-        </div>
-
-        <div class="col-md-3 text-center">
-        <span>日付</span>
-        <div class="blog-inner" style="border:solid 1px; ">
-          <img class="img-responsive" src="user_report_img/<?php echo $report_each['img_name']; ?>" alt="Blog" style="margin: 5px">
-          <p style="text-align: left; padding: 3px 3px"><?php echo $report_each['feed']; ?></p>
-          <div class="desc">
-
-            <form method="POST" action="">
-              <div class="row">
-                <div class="col" style="padding: 0px">
-                  <a href="#"><button type="button" class="btn btn-warning" style="font-size: 13px">お気に入りから削除</button></a>
-                </div>
-                <div class="col">
-                  <a href="#"><button type="button" class="btn btn-primary" style="font-size: 13px">レッスン詳細ページへ</button></a>
-                </div>
-              </div>
-            </form>
-
-          </div>
-        </div>
-        </div>
-
-        <div class="col-md-3 text-center">
-        <span>日付</span>
-        <div class="blog-inner" style="border:solid 1px; ">
-          <img class="img-responsive" src="user_report_img/<?php echo $report_each['img_name']; ?>" alt="Blog" style="margin: 5px">
-          <p style="text-align: left; padding: 3px 3px"><?php echo $report_each['feed']; ?></p>
-          <div class="desc">
-
-            <form method="POST" action="">
-              <div class="row">
-                <div class="col" style="padding: 0px">
-                  <a href="#"><button type="button" class="btn btn-warning" style="font-size: 13px">お気に入りから削除</button></a>
-                </div>
-                <div class="col">
-                  <a href="#"><button type="button" class="btn btn-primary" style="font-size: 13px">レッスン詳細ページへ</button></a>
-                </div>
-              </div>
-            </form>
-
-          </div>
-        </div>
-        </div>
 
     </div>
 
